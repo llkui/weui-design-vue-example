e050b819801a9e93a80448e42851682f8d82945c
llkui <llkui241569@163.com>
2021-01-22 14:31:09 +0800

    add weui-design-vue-example-ts

--- README.md
+++ README.md
@@ -1,14 +1,38 @@
-# weui-design-vue-example
+# Weui Design Vue 示例
 
-## Links
+## 目录
 
-- [Documentation](https://llkui.github.io/weui-design-vue/#/introduce)
+### vue2
 
-## Development
+vue2 目录示范了如何用 Vue 2 和 Weui Design Vue 2 搭建Weui示例页面，包含如下功能：
 
-```bash
-$ git clone git@github.com:llkui/weui-design-vue-example.git
-$ cd weui-design-vue-example
-$ npm install
-$ npm run serve
+- 基于 Weui Design Vue 搭建
+- 基于 vue-router 的单页面应用
+
+> 技术栈：Vue2、Weui Design Vue 2、Vue Cli、Vue Router
+
+### vue2Ts
+
+vue2 目录示范了如何用 Vue 2 和 Weui Design Vue 2 搭建Weui示例页面，包含如下功能：
+
+- 基于 Weui Design Vue 搭建
+- 基于 Typescript 搭建
+- 基于 vue-router 的单页面应用
+
+> 技术栈：Vue、Weui Design Vue 2、Vue Cli、Vue Router、Typescript
+
+### 开发命令
+
+```base
+# 安装依赖
+# 注意：请切换到对应的子目录下安装
+cd vue2
+npm install
+
+# 本地开发
+# 通过 localhost:8080/weui-design-vue-example 访问页面
+npm run serve
+
+# 生产环境构建
+npm run build
 ```
---
+++ vue2Ts/src/views/Dialog.vue
@@ -0,0 +1,93 @@
+<template>
+  <Page title="Dialog" desc="对话框" type="white">
+    <weui-button type="default" @click="showDialog('ios1')"
+      >iOS Dialog样式一</weui-button
+    >
+    <weui-button type="default" @click="showDialog('ios2')"
+      >iOS Dialog样式二</weui-button
+    >
+    <weui-button type="default" @click="showDialog('android1')"
+      >Android Dialog样式一</weui-button
+    >
+    <weui-button type="default" @click="showDialog('android2')"
+      >Android Dialog样式二</weui-button
+    >
+    <weui-dialog
+      :visible="isVisible"
+      :type="type"
+      :title="title"
+      :cancelText="cancelText"
+      :okText="okText"
+      :showCancel="showCancel"
+      @onCancel="handleCancel($event)"
+      @onOk="handleOk($event)"
+    >
+      {{ content }}
+    </weui-dialog>
+  </Page>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import Page from '@/components/Page.vue'
+
+export default Vue.extend({
+  components: { Page },
+  data() {
+    return {
+      isVisible: false,
+      type: 'ios',
+      title: '',
+      content: '',
+      cancelText: '',
+      okText: '',
+      showCancel: true,
+    }
+  },
+  methods: {
+    showDialog(type: string) {
+      this.isVisible = true
+      switch (type) {
+        case 'ios1':
+          this.type = 'ios'
+          this.title = '弹窗标题'
+          this.content =
+            '弹窗内容，告知当前状态、信息和解决方法，描述文字尽量控制在三行内'
+          this.cancelText = '辅助操作'
+          this.okText = '主操作'
+          break
+        case 'ios2':
+          this.type = 'ios'
+          this.title = ''
+          this.content =
+            '弹窗内容，告知当前状态、信息和解决方法，描述文字尽量控制在三行内'
+          this.showCancel = false
+          this.cancelText = ''
+          this.okText = '知道了'
+          break
+        case 'android1':
+          this.type = 'android'
+          this.title = '弹窗标题'
+          this.content =
+            '弹窗内容，告知当前状态、信息和解决方法，描述文字尽量控制在三行内'
+          this.cancelText = '辅助操作'
+          this.okText = '主操作'
+          break
+        case 'android2':
+          this.type = 'android'
+          this.title = ''
+          this.content =
+            '弹窗内容，告知当前状态、信息和解决方法，描述文字尽量控制在三行内'
+          this.cancelText = '辅助操作'
+          this.okText = '主操作'
+          break
+      }
+    },
+    handleCancel() {
+      this.isVisible = false
+    },
+    handleOk() {
+      this.isVisible = false
+    },
+  },
+})
+</script>
