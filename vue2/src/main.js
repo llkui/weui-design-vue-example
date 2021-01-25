bc8f2febef57cabe68695339b66bec039b56e090
llkui <llkui241569@163.com>
2021-01-25 10:08:28 +0800

    update weui-design-vue 2.0.0

--- vue2/src/main.js
+++ vue2/src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
-import WeuiVue from 'weui-design-vue'
+import WeuiDesignVue from 'weui-design-vue'
 import 'weui-design-vue/lib/weui-design-vue.css'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
 
-Vue.use(WeuiVue)
+Vue.use(WeuiDesignVue)
 
 new Vue({
   router,
